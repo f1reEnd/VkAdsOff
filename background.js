517d0b6bf4d00cfffe7dca7fb2f15e45da689be4
unknown <mainfireend@gmail.com>
2022-05-07 22:10:27 +0300

    bug fix

--- background.js
+++ background.js
@@ -10,14 +10,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ "buttonStatus": "off" });
     console.log("[onInstalled] A button status variable has been created");
     console.log("[onInstalled] the default status of the button is \"off\"");
-});
-
-chrome.storage.sync.get("buttonStatus", ({ buttonStatus }) => {
-    if (buttonStatus == "off") {
-        offIcon();
-    } else {
-        onIcon();
-    }
+    offIcon();
 });
 
 
